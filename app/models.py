d0e2fdc4a45f17d9f116361364993699833b865c
dbyta <dbyf.rguez@gmail.com>
2022-12-21 23:12:31 -0500

    feat: login user/models modification

--- app/models.py
+++ app/models.py
@@ -1,13 +1,24 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Patiente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +44,24 @@ class Patiente(db.Model):
     edadGroup = db.Column(db.Integer, index=True)
     factorGroup = db.Column(db.Integer, index=True)
     donor = db.Column(db.Boolean, index=True)
+    chargesheet_id = db.relationship("ChargeSheet", backref= 'patiente', lazy = 'dynamic')
+    resume_id = db.relationship("Resume", backref= 'patiente', lazy = 'dynamic')
+    ocupatHist_id = db.relationship("OcupationalHistory", backref= 'patiente', lazy = 'dynamic')
+    iniHealthState_id = db.relationship("InitialHealthState", backref= 'patiente', lazy = 'dynamic')
+    pathologicalHistory_id = db.relationship("PathologicalHistory", backref= 'patiente', lazy = 'dynamic')
+    initialSanitaryControl = db.relationship("InitialSanitaryControl", backref= 'patiente', lazy = 'dynamic')
+    vacunation_id = db.relationship("Vacunation", backref= 'patiente', lazy = 'dynamic')
+    geneticsRisks_id = db.relationship("GeneticsRisks", backref= 'patiente', lazy = 'dynamic')
+    expositionRisks_id = db.relationship("ExpositionRisks", backref= 'patiente', lazy = 'dynamic')
+    cronicsIlness_id = db.relationship("CronicsIlness", backref= 'patiente', lazy = 'dynamic')
+    incidents_id = db.relationship("Incidents", backref= 'patiente', lazy = 'dynamic')
+    psicological_id = db.relationship("Psicological", backref= 'patiente', lazy = 'dynamic')
+    bodyExamn_id = db.relationship("BodyExamn", backref= 'patiente', lazy = 'dynamic')
+    complementary_id = db.relationship("Complementary", backref= 'patiente', lazy = 'dynamic')
+    arterialTension_id = db.relationship("ArterialTension", backref= 'patiente', lazy = 'dynamic')
+    donor_id = db.relationship("Donor", backref= 'patiente', lazy = 'dynamic')
+    vision_id = db.relationship("Vision", backref= 'patiente', lazy = 'dynamic')
+    medicCheck_id = db.relationship("MedicCheck", backref= 'patiente', lazy = 'dynamic')
     
     def __repr__(self):
         return '<Patiente {}>'.format(self.firstname + self.lastname)
@@ -45,16 +74,16 @@ class ChargeSheet(db.Model):
     reaction = db.Column(db.String(64), index = True)
     indicatedDrugs = db.Column(db.String(64), index = True)
     evolution = db.Column(db.String(200), index = True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class Resume(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, index = True)    
     dispensaryGroup = db.Column(db.String(64), index = True)
     valoration = db.Column(db.String(200), index = True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class OcupationalHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -65,25 +94,25 @@ class OcupationalHistory(db.Model):
     currentOccupation = db.Column(db.String(64), index = True)
     currentOccupationSince = db.Column(db.Date, index = True)
     currentOccupationTo = db.Column(db.Date, index = True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class InitialHealthState(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     drugAlergy = db.Column(db.String(64), index = True)
     tetanusVaccionelastActivation = db.Column(db.Date, index=True)
     tetanusVaccionenextActivation = db.Column(db.Date, index=True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
-class PahtolocalHistory(db.Model):
+class PathologicalHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pathologies = db.Column(db.String(20), index = True)
     othersPathologies = db.Column(db.String(64), index = True)
     professionalIlness = db.Column(db.String(64), index = True)
     surgeries = db.Column(db.String(64), index = True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class InitialSanitaryControl(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -97,22 +126,22 @@ class InitialSanitaryControl(db.Model):
     return3MonthControl = db.Column(db.Boolean, index = True)
     returneMothControlDate = db.Column(db.Date, index = True)
     controls3Month = db.Column(db.String(6), index = True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class Vacunation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     vaccines = db.Column(db.String(11), index = True)
     anothers = db.Column(db.String(64), index = True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class GeneticsRisks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, index = True)
     risks = db.Column(db.String(4), index=True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class ExpositionRisks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -122,16 +151,16 @@ class ExpositionRisks(db.Model):
     non_ionazinRadiations = db.Column(db.String(4), index=True)
     ionazingRadiations = db.Column(db.String(6), index=True)
     expRadiometricEvaluation = db.Column(db.String(1), index=True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
     
 class CronicsIlness(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, index = True)
     ilness = db.Column(db.String(20), index=True)
     anothers = db.Column(db.String(64), index=True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 #UNFINISH
 class Incidents(db.Model):
@@ -146,8 +175,8 @@ class Incidents(db.Model):
     medicalCerificatedTo = db.Column(db.Date, index=True)
     medicalcertificatedCauses = db.Column(db.String(64), index=True)
     medicalCertificatedFrom = db.Column(db.String(64), index=True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class Psicological(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -155,8 +184,8 @@ class Psicological(db.Model):
     workFeelings = db.Column(db.String(64), index = True)
     stressLevel = db.Column(db.String(64), index = True)
     symptoms = db.Column(db.String(64), index = True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
     
 class BodyExamn(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -184,8 +213,8 @@ class BodyExamn(db.Model):
     pelvicGirdleLowerLimbs = db.Column(db.String(64), index=True)
     spine = db.Column(db.String(64), index=True)
     neurologicStats = db.Column(db.String(64), index=True)    
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
     
 class Complementary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -228,8 +257,8 @@ class Complementary(db.Model):
     adiometricaTest= db.Column(db.String(64), index=True)
     Rx= db.Column(db.String(64), index=True)
     EKG= db.Column(db.String(64), index=True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
    
 class ArterialTension(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -237,14 +266,14 @@ class ArterialTension(db.Model):
     TAS= db.Column(db.Float, index=True)
     TDH= db.Column(db.Float, index=True)
     position= db.Column(db.String(64), index=True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
    
 class Donor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, index = True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class Vision(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -254,8 +283,8 @@ class Vision(db.Model):
     OI= db.Column(db.String(64), index=True)
     results= db.Column(db.String(64), index=True)
     diagnostic= db.Column(db.String(64), index=True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
 
 class MedicCheck(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -273,5 +302,5 @@ class MedicCheck(db.Model):
     correctiveAction= db.Column(db.String(64), index=True)
     nextMedicCheck = db.Column(db.Date, index=True)
     check= db.Column(db.Boolean, index=True)
-    patiente = db.Column(db.Integer, db.ForeignKey('patiente.id'))
-    patiente = db.relationship("Patiente", backref= db.backref("patiente", uselist=False))
+    patiente_id = db.Column(db.Integer, db.ForeignKey('patiente.id'))
+    
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from app import app, db
-from app.models import User, CronicsIlness,Vision,MedicCheck,ArterialTension,Donor,Complementary, BodyExamn,Psicological,Incidents,Patiente,ExpositionRisks,GeneticsRisks,Vacunation, PahtolocalHistory,OcupationalHistory, InitialSanitaryControl, InitialHealthState, ChargeSheet, Resume
+from app.models import User, CronicsIlness,Vision,MedicCheck,ArterialTension,Donor,Complementary, BodyExamn,Psicological,Incidents,Patiente,ExpositionRisks,GeneticsRisks,Vacunation, PathologicalHistory,OcupationalHistory, InitialSanitaryControl, InitialHealthState, ChargeSheet, Resume
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Patiente': Patiente,
             'ChargeSheet': ChargeSheet, 'Resume': Resume,
             'OcupationalHistory': OcupationalHistory, 'InitialHealthState': InitialHealthState,
-            'PahtolocalHistory': PahtolocalHistory, 'InitialSanitaryControl': InitialSanitaryControl,
+            'PahtolocalHistory': PathologicalHistory, 'InitialSanitaryControl': InitialSanitaryControl,
             'Vacunation': Vacunation, 'GeneticsRisks': GeneticsRisks,
             'ExpositionRisks': ExpositionRisks, 'CronicsIlness': CronicsIlness,
             'Incidents': Incidents, 'Psicological': Psicological,
