eaa8cbb4b62d82e5009b089dc2fd7215117c5e68
dbyta <dbyf.rguez@gmail.com>
2022-12-22 07:07:47 -0500

    feat: reset password email implem NOT-working

--- app/form.py
+++ app/form.py
@@ -1,13 +1,24 @@
 from flask_wtf import FlaskForm
 from .constants import *
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, SelectField, IntegerField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, NumberRange, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
 
 class PatienteForm(FlaskForm):
     ci = StringField('CI', validators=[DataRequired()])
